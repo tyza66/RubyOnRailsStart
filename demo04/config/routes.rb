24f29b71ba74a111a5301d832c9a164eee134066
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-07-21 09:42:12 +0800

    新建04

---
+++ demo04/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get "welcome/say_hello" => "welcome#say"
+  get "welcome/index" => "welcome#index"
+  root :to => "welcome#index"
+end
