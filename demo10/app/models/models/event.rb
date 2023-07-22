94e888a9a904ef8f9a10129b5cdb2e9945f183cf
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-07-22 08:50:27 +0800

    替换旧版

--- demo10/app/models/models/event.rb
+++
@@ -1,3 +0,0 @@
-class Event < ApplicationRecord
-  validates_presence_of :name  #这样写就是说明name属性不能为空
-end
--- demo10/config/routes.rb
+++ demo10/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :people #像这样定义的就相当于将controller中定义的那些全弄进路由
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  match ':controller(/:action(/:id(.:format)))', :via => :all
+  get "welcome/say_hello" => "welcome#say"
+  get "welcome/index" => "welcome#index"
+  root :to => "welcome#index"
+  match ':controller(/:action(/:id(.:format)))', :via => :all  #这里定义了外卡路由，可以将我们的两层的和带参数的请求映射到请求地址上
 end
